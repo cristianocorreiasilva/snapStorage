0400898e25d95be346f88aff4ce8ac17c6f5d368
cristianocorreiasilva <cristianocorreiadasilva81@gmail.com>
2026-04-17 19:21:52 -0300

    Layout e estáticos corrigidos, pronto para deploy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 require('dotenv').config(); // LINHA 1: Carrega as variáveis primeiro
+
 const Express = require("express");
+const {engine} = require("express-handlebars")
+
+const path = require("path");
+
 const connection = require("./config/db"); // LINHA 3: Só chama o banco DEPOIS
 
 const app = Express();
 
+app.set("views", path.join(__dirname, "views"));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+app.use(Express.static(path.join(__dirname, "views")));
+
+// Servindo a pasta dist do bootstrap como se fosse a pasta "/bootstrap" na URL
+app.use("/bootstrap", Express.static(path.join(__dirname, "..", "node_modules", "bootstrap", "dist")));
+
+app.use(Express.static(path.resolve(__dirname, "public")));
+
 app.get("/", (req, res)=>{
-    res.send("No ar ...");
+    res.render("home");
 });
 
 const PORT = process.env.port || 3000;
